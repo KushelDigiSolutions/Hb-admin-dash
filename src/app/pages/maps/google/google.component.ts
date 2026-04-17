2c6730eb02b9518c1f684243d36f3ec4f27d0e96
Karankartik07 <unknownkartik07@gmail.com>
2026-04-17 15:59:54 +0530

    fixed deployment and build

--- src/app/pages/maps/google/google.component.ts
+++ src/app/pages/maps/google/google.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild, Input, Inject, PLATFORM_ID } from '@angular/core';
-
-import { MapsAPILoader } from '@agm/core';
-
-import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: false,
@@ -19,44 +15,18 @@ export class GoogleComponent implements OnInit {
   latitude = 52.128973;
   markers: any;
 
-  @ViewChild('streetviewMap', { static: true }) streetviewMap: any;
-  @ViewChild('streetviewPano', { static: true }) streetviewPano: any;
-
   // bread crumb items
   breadCrumbItems: Array<{}>;
-  constructor(@Inject(PLATFORM_ID) private platformId: any, private mapsAPILoader: MapsAPILoader) { }
 
+  constructor() { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Maps' }, { label: 'Google Maps', active: true }];
-
-    this._initPanorama();
 
     /**
      * fetches data
      */
     this._fetchData();
-  }
-
-  /**
-   * street view map
-   */
-  _initPanorama() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.mapsAPILoader.load().then(() => {
-        const center = { lat: 42.345573, lng: -71.098326 };
-        // tslint:disable-next-line: no-string-literal
-        const map = new window['google'].maps.Map(this.streetviewMap.nativeElement, { center, zoom: 12, scrollwheel: false });
-        // tslint:disable-next-line: no-string-literal
-        const panorama = new window['google'].maps.StreetViewPanorama(
-          this.streetviewPano.nativeElement, {
-          position: center,
-          pov: { heading: 34, pitch: 10 },
-          scrollwheel: false
-        });
-        map.setStreetView(panorama);
-      });
-    }
   }
 
   /**
